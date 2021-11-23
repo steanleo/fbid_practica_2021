ec199e72da56cd07eecbb6e3e09f5d93498d7ce7
steanleo <sa.leont94@gmail.com>
2021-11-23 15:49:51 +0100

    v1.0

---
+++ kafka/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties &
+systemctl enable zookeeper.service
+systemctl enable kafka.service
+systemctl start zookeeper.service
+systemctl start kafka.service
+kafka-topics.sh \
+      --create \
+      --bootstrap-server 172.20.0.3:9092 \
+      --replication-factor 1 \
+      --partitions 1 \
+      --topic flight_delay_classification_request
+systemctl stop kafka.service
+kafka-server-start.sh $KAFKA_HOME/config/server.properties 
